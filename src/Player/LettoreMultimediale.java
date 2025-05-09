4f669315bdbc87b27162fb65c60d5ac10ffc0610
davidelong0 <davidelong0@hotmail.com>
2025-05-09 16:33:39 +0200

    player finito
    introdotta gestione errori e gestito meglio le differenze tra i tre elementi

--- src/Player/LettoreMultimediale.java
+++ src/Player/LettoreMultimediale.java
@@ -2,7 +2,6 @@ package Player;
 
 import java.util.Scanner;
 
-
 public class LettoreMultimediale {
 
     private ElementoMultimediale[] elementi = new ElementoMultimediale[5];
@@ -11,7 +10,7 @@ public class LettoreMultimediale {
 
         elementi[0] = new Video("Video 1", 5, 3, 2);
         elementi[1] = new RegistrazioneAudio("Audio 1", 4, 3);
-        elementi[2] = new Video("Video 2", 7, 5, 4);
+        elementi[2] = new Immagine("Immagine 1", 6);
         elementi[3] = new RegistrazioneAudio("Audio 2", 6, 2);
         elementi[4] = new Video("Video 3", 10, 7, 5);
     }
@@ -19,15 +18,23 @@ public class LettoreMultimediale {
     public void interagisciConUtente() {
         Scanner scanner = new Scanner(System.in);
 
-
         while (true) {
-
-            System.out.println("Scegli un elemento da riprodurre (1-5) o 0 per uscire:");
+            System.out.println("Scegli un elemento da eseguire (1-5) o 0 per uscire:");
             for (int i = 0; i < elementi.length; i++) {
                 System.out.println((i + 1) + ". " + elementi[i].getTitolo());
             }
 
-            int scelta = scanner.nextInt();
+            System.out.print("Tua scelta: ");
+            String input = scanner.nextLine();
+
+            int scelta;
+
+            try {
+                scelta = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Input non valido. Inserisci un numero intero.");
+                continue;
+            }
 
             if (scelta == 0) {
                 System.out.println("Uscita dal lettore.");
@@ -35,14 +42,17 @@ public class LettoreMultimediale {
             }
 
             if (scelta >= 1 && scelta <= 5) {
+                ElementoMultimediale elemento = elementi[scelta - 1];
 
-                elementi[scelta - 1].play();
+                if (elemento instanceof Immagine) {
+                    ((Immagine) elemento).show();
+                } else if (elemento instanceof Riproduci) {
+                    ((Riproduci) elemento).play();
+                }
             } else {
-
                 System.out.println("Scelta non valida, riprova.");
             }
         }
-
 
         scanner.close();
     }
